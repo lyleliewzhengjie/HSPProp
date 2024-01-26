86a0864a91e9747869a8c14185ebf58181a2b2e5
zhengjieliewUCAM <138018082+zhengjieliewUCAM@users.noreply.github.com>
2024-01-26 02:12:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,27 @@ import joblib
 import random
 
 def get_user_input():
-    # Create widgets for each feature
-    polymer_repeating_unit_mw = st.number_input('Polymer Repeating Unit Molecular Weight')
-    polymer_mw = st.number_input('Polymer Molecular Weight')
-    polymer_logp = st.number_input('Polymer LogP')
-    tpsa = st.number_input('TPSA')
-    num_h_donor = st.number_input('Number of H Donor')
-    solvent_mw = st.number_input('Solvent Molecular Weight')
-    solvent_logp = st.number_input('Solvent LogP')
-    solvent_dielectric_constant = st.number_input('Solvent Dielectric Constant')
-    solvent_dipole_moments = st.number_input('Solvent Dipole Moments')
-    solvent_viscosity = st.number_input('Solvent Viscosity')
-    solvent_dd = st.number_input('Solvent δD')
-    solvent_dp = st.number_input('Solvent δP')
-    solvent_dh = st.number_input('Solvent δH')
+    # Create two columns for inputs
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header('Polymer Properties')
+        polymer_repeating_unit_mw = st.number_input('Polymer Repeating Unit Molecular Weight')
+        polymer_mw = st.number_input('Polymer Molecular Weight')
+        polymer_logp = st.number_input('Polymer LogP')
+        tpsa = st.number_input('TPSA')
+        num_h_donor = st.number_input('Number of H Donor')
+
+    with col2:
+        st.header('Solvent Properties')
+        solvent_mw = st.number_input('Solvent Molecular Weight')
+        solvent_logp = st.number_input('Solvent LogP')
+        solvent_dielectric_constant = st.number_input('Solvent Dielectric Constant')
+        solvent_dipole_moments = st.number_input('Solvent Dipole Moments')
+        solvent_viscosity = st.number_input('Solvent Viscosity')
+        solvent_dd = st.number_input('Solvent δD')
+        solvent_dp = st.number_input('Solvent δP')
+        solvent_dh = st.number_input('Solvent δH')
 
     # Create a DataFrame from the inputs (if you need to use these inputs)
     # features = pd.DataFrame([[...]], columns=[...])
@@ -26,7 +33,7 @@ def get_user_input():
     return pd.DataFrame()
 
 def main():
-    st.title('Polymer Solubility Prediction App')
+    st.title('Solubility Prediction App')
 
     input_df = get_user_input()
 
@@ -38,7 +45,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 # Main function for the Streamlit app
 # def main():
 #    st.title('Predicting if a Solvent Dissolves a Polymer')
